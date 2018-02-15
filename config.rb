ef1b7279d5a94950ea9ed03b05ab5ad42fa46f43
Kevin Burnett <kwburnett@gmail.com>
2018-02-15 14:24:06 +0300

    adding Compass to the project for easier CSS management

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = '/'
+sass_dir = 'theme/openmrs/sass'
+css_dir = 'theme/openmrs/css'
+images_dir = 'theme/openmrs/images'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
